067dd8d4bf7afb03e6091bf2044b06000b33fd65
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-03-31 12:56:52 -0500

    Se actualiza video de intro.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -14,7 +14,7 @@
 
     figure(data-aos="zoom-out")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/XGSPIaLtJ-M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/fh8LtYRvL3s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     
 </template>
